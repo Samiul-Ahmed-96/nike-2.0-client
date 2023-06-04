15c9ba67a9ef7a63539e3f1ab817086baf18e270
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2023-06-04 17:36:44 +0600

    Added menu

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,24 @@
+import Link from "next/link";
+import { useState } from "react";
+import Menu from "./Menu";
+import Wrapper from "./Wrapper";
 
 const Header = () => {
-    return <div className="text">Header</div>;
+
+    const [mobileMenu , setMobileMenu] = useState(false);
+    const [showCatMenu ,setShowCatMenu  ] = useState(false);
+    const [show , setShow] =  useState("translate-y-0");
+    const [lastScrollY , setLastScrollY] = useState(0);
+
+    return <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}>
+        <Wrapper className="h-[60px] flex justify-between items-center">
+            <Link href={'/'}>
+                <img src="/logo.svg" alt="logo"  className="w-[40px] md:w-[60px]"/>
+            </Link>
+
+            <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu}/>
+        </Wrapper>
+    </header>;
 };
 
 export default Header;
