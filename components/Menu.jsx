f12684707d3c5162b0c7adda812a10db265367cc
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2023-06-04 21:50:28 +0600

    Added mobile menu

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -27,12 +27,12 @@ const Menu = ({showCatMenu , setShowCatMenu}) => {
                     <BsChevronDown size={14}/>
                     {
                         showCatMenu && (
-                            <ul className="bg-white absolute top-6 left-0 min-w-[250px] px-1 text-black shadow-lg">
+                            <ul className="bg-white absolute top-6 left-0 min-w-[250px] px-1 text-black shadow-lg rounded-md">
                                 {
                                     subMenuData.map((submenu) =>{
                                         return (
-                                            <Link key={submenu.id} href={'/'}> 
-                                                <li className="flex items-center justify-between h-12 px-3 py-1 hover:bg-black/[0.03] rounded-md">
+                                            <Link key={submenu.id} href={'/'} onClick={()=> setShowCatMenu(false)}> 
+                                                <li className="flex items-center justify-between h-12 px-3 py-1 my-1 hover:bg-black/[0.03] rounded-md">
                                                 {submenu.name}
                                                 <span className="opacity-50 text-sm">{submenu.doc_count}</span>
                                                 
