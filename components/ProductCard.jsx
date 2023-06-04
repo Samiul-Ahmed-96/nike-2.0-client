dca92d5bae76e00c1858de73e906b1027b9d3119
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2023-06-04 23:39:40 +0600

    Added product card

---
+++ components/ProductCard.jsx
@@ -0,0 +1,30 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const ProductCard = () => {
+  return (
+    <Link
+    href={`/product/1`}
+    className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer"
+>
+    <Image
+        width={500}
+        height={500}
+        src={'/product-1.webp'}
+        alt={'product-image'}
+    />
+    <div className="p-4 text-black/[0.9]">
+        <h2 className="text-lg font-medium">Product Name</h2>
+        <div className="flex items-center text-black/[0.5]">
+            <p className="mr-2 text-lg font-semibold">
+                $ 234
+            </p>
+
+            
+        </div>
+    </div>
+</Link>
+  )
+}
+
+export default ProductCard
